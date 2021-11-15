db181ffa66b247a75dd9f4449d4f1006007b8495
ryuash <ryuash123@gmail.com>
2021-11-15 16:03:49 +0800

    setup: nonce account

--- src/screens/account_details/components/nonce_account/index.tsx
+++ src/screens/account_details/components/nonce_account/index.tsx
@@ -3,6 +3,10 @@ import {
   Overview,
 } from './components';
 import { useStyles } from './styles';
+import {
+  Tokens,
+  Transactions,
+} from '..';
 
 const NonceAccount = () => {
   const classes = useStyles();
@@ -14,10 +18,33 @@ const NonceAccount = () => {
       blockhash: '9hZMP3JunShlriadsrvnsrlquqwt7suvspudasvimqai',
       fee: 3453,
     },
+    accounts: {
+      stake: Array(15).fill('desmos1rzhewpmmdl72lhnxj6zmxr4v94f522s4ff2psv'),
+      vote: Array(3).fill('desmos1rzhewpmmdl72lhnxj6zmxr4v94f522s4ff2psv'),
+      nonce: Array(100).fill('desmos1rzhewpmmdl72lhnxj6zmxr4v94f522s4ff2psv'),
+      token: Array(10).fill('desmos1rzhewpmmdl72lhnxj6zmxr4v94f522s4ff2psv'),
+    },
+    tokens: {
+      tokens: Array(14).fill({
+        token: 'ETH',
+        amount: 243530,
+      }),
+    },
   };
   return (
     <>
       <Overview className={classes.overview} {...data.overview} />
+      <Tokens
+        className={classes.tokens}
+        {...data.tokens}
+      />
+      <Transactions
+        className={classes.transactions}
+        loadNextPage={() => null}
+        data={[]}
+        hasNextPage={false}
+        isNextPageLoading={false}
+      />
     </>
   );
 };
