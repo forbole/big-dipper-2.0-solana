32dd200cf5f2edfc070bfc12caa40c130ca6396d
Ellie <isabelleya927@gmail.com>
2021-12-07 15:22:55 +0800

    update format for epoch number and rate in hooks

--- src/screens/home/components/epoch/hooks.tsx
+++ src/screens/home/components/epoch/hooks.tsx
@@ -5,15 +5,17 @@ import {
   useEpochQuery,
   EpochQuery,
 } from '@graphql/types';
+import Big from 'big.js';
+import { formatNumber } from '@utils/format_token';
 
 export const useEpoch = () => {
   const [state, setState] = useState<{
-    epochRate: number;
-    epochNumber: number;
+    epochRate: string;
+    epochNumber: string;
     epochTime: number;
   }>({
-    epochRate: 0,
-    epochNumber: 0,
+    epochRate: '0',
+    epochNumber: '0',
     epochTime: 0,
   });
 
@@ -30,12 +32,16 @@ export const useEpoch = () => {
 
     const averageSlotTime = R.pathOr(0, ['average_slot_time_per_hour', 0, 'average_time'], data);
     const slot = R.pathOr(0, ['block', 0, 'slot'], data);
+
     const epochNumber = slot / 432000;
-    results.epochNumber = numeral(epochNumber).value();
+    results.epochNumber = formatNumber(Big(epochNumber).toPrecision(), 0);
+
     const epochTime = 432000 - (slot % 432000) * averageSlotTime;
     results.epochTime = numeral(epochTime).value();
+
     const epochRate = (432000 - (slot % 432000)) / 432000;
-    results.epochRate = numeral(epochRate).value() * 100;
+    results.epochRate = formatNumber(Big(epochRate).times(100).toPrecision(), 2);
+
     console.log('slot % 432000', slot % 432000);
 
     return results;
