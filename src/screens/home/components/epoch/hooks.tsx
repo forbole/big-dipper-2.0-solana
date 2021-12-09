0a18c89a6576a29b08639326801df3442b5e56b6
Ellie <isabelleya927@gmail.com>
2021-12-09 14:18:17 +0800

    rmv unused console.log

--- src/screens/home/components/epoch/hooks.tsx
+++ src/screens/home/components/epoch/hooks.tsx
@@ -26,24 +26,16 @@ export const useEpoch = () => {
 
   const formatEpoch = (data: EpochQuery) => {
     const results = { ...state };
-    console.log('results', results);
-    console.log('state', state);
-    console.log('data', data);
 
     const slotsPerEpoch = 432000;
     const averageSlotTime = R.pathOr(0, ['average_slot_time_per_hour', 0, 'average_time'], data);
     const slot = R.pathOr(0, ['block', 0, 'slot'], data);
 
     const epochNumber = slot / slotsPerEpoch;
-    results.epochNumber = formatNumber(Big(epochNumber).toPrecision(), 0);
-
-    console.log('toFixed = none', formatNumber(Big(epochNumber).toPrecision()));
-    console.log('toFixed = 0', formatNumber(Big(epochNumber).toPrecision(), 0));
-    console.log('toFixed = 1', formatNumber(Big(epochNumber).toPrecision(), 1));
-    console.log('toFixed = 2', formatNumber(Big(epochNumber).toPrecision(), 2));
+    results.epochNumber = formatNumber(Big(epochNumber).toPrecision());
 
     const epochRate = (slotsPerEpoch - (slot % slotsPerEpoch)) / slotsPerEpoch;
-    results.epochRate = formatNumber(Big(epochRate).times(100).toPrecision(), 0);
+    results.epochRate = formatNumber(Big(epochRate).times(100).toPrecision());
 
     const epochTimeRaw = (slotsPerEpoch - (slot % slotsPerEpoch)) * averageSlotTime;
     const epochTimeHour = Math.floor(epochTimeRaw / (60 * 60));
