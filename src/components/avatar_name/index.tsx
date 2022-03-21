ca1bd14326cf33c5e309b68d1f7d187816f5a63c
yayay927 <isabelleya927@gmail.com>
2022-03-21 13:18:42 +0000

    update avatarname as review suggestion

--- src/components/avatar_name/index.tsx
+++ src/components/avatar_name/index.tsx
@@ -10,13 +10,6 @@ const AvatarName: React.FC<AvatarName> = ({
   className, address, name, imageUrl, href = ACCOUNT_DETAILS,
 }) => {
   const classes = useStyles();
-  let avatarName = name;
-
-  if (name === '""') {
-    avatarName = address;
-  } else if (name.length === 1) {
-    avatarName = address;
-  }
 
   return (
     <Link href={href(address)}>
@@ -24,7 +17,7 @@ const AvatarName: React.FC<AvatarName> = ({
         <div className={classnames(className, classes.root)}>
           <Avatar address={address} imageUrl={imageUrl} />
           <Typography variant="body1">
-            {name ? avatarName : address}
+            {name || address}
           </Typography>
         </div>
       </a>
