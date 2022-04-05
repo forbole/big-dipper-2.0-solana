c88e1442bd3006268d294600c140feef5f0c081d
yayay927 <isabelleya927@gmail.com>
2022-04-05 14:25:53 +0100

    remove consolelog

--- src/screens/validators/components/list/components/desktop/hooks.ts
+++ src/screens/validators/components/list/components/desktop/hooks.ts
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 
 export const useDesktop = (tab: number, ref: any) => {
-  // declare ref type as any because React.RefObject<unknown> shows folling error
-  // Property 'scrollToItem' does not exist on type 'unknown'
   useEffect(() => {
     if (ref.current != null) {
       ref.current.scrollToItem({
