37fe925cb3c2638bae266cd9318d656133510a3f
ryuash <ryuash123@gmail.com>
2022-04-05 10:41:13 -0400

    wip: working on system program

--- src/models/index.ts
+++ src/models/index.ts
@@ -14,6 +14,9 @@ import InstructionUnknown from './instructions/instruction_unknown';
 // system program
 import InstructionCreateAccount from './instructions/system_program/create_account';
 import InstructionTransfer from './instructions/system_program/transfer';
+import SystemProgramAssign from './instructions/system_program/assign';
+import SystemProgramCreateAccountWithSeed from './instructions/system_program/create_account_with_seed';
+import SystemProgramAdvancedNonce from './instructions/system_program/advance_nonce';
 
 // token program
 import TokenProgramApprove from './instructions/token_program/approve';
@@ -21,6 +24,13 @@ import TokenProgramRevoke from './instructions/token_program/revoke';
 import TokenProgramInitializeMint from './instructions/token_program/initialize_mint';
 import TokenProgramInitializeAccount from './instructions/token_program/initialize_account';
 import TokenProgramTransfer from './instructions/token_program/transfer';
+import TokenProgramTransferChecked from './instructions/token_program/transfer_checked';
+import TokenProgramApproveChecked from './instructions/token_program/approve_checked';
+import TokenProgramSetAuthority from './instructions/token_program/set_authority';
+import TokenProgramMintTo from './instructions/token_program/mint_to';
+import TokenProgramBurn from './instructions/token_program/burn';
+import TokenProgramCloseAccount from './instructions/token_program/close_account';
+import TokenProgramFreezeAccount from './instructions/token_program/freeze_account';
 
 // vote Program
 import VoteProgramVote from './instructions/vote_program/vote';
@@ -46,12 +56,22 @@ export {
   // system program
   InstructionCreateAccount,
   InstructionTransfer,
+  SystemProgramAssign,
+  SystemProgramCreateAccountWithSeed,
+  SystemProgramAdvancedNonce,
   // token program
   TokenProgramApprove,
   TokenProgramRevoke,
   TokenProgramInitializeMint,
   TokenProgramInitializeAccount,
   TokenProgramTransfer,
+  TokenProgramTransferChecked,
+  TokenProgramApproveChecked,
+  TokenProgramSetAuthority,
+  TokenProgramMintTo,
+  TokenProgramBurn,
+  TokenProgramCloseAccount,
+  TokenProgramFreezeAccount,
   // vote program
   VoteProgramVote,
   VoteProgramInitializeAccount,
