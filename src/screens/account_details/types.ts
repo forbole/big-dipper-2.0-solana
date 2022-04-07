dbef5fab604291979cc78750481fbb45d0d14d16
ryuash <ryuash123@gmail.com>
2022-04-07 10:48:44 -0400

    fix build

--- src/screens/account_details/types.ts
+++ src/screens/account_details/types.ts
@@ -4,3 +4,44 @@ export type AccountDetailState = {
   desmosProfile: DesmosProfile | null;
   accountType: string;
 }
+
+// delete later
+export type BalanceType = {
+  native: TokenUnit;
+  stake: TokenUnit;
+  nonce: TokenUnit;
+  vote: TokenUnit;
+  total: TokenUnit;
+}
+
+export type OtherTokenType = {
+  denom: string;
+  available: TokenUnit;
+  reward: TokenUnit;
+  commission: TokenUnit;
+}
+
+export type DelegationType = {
+  validator: string;
+  validatorStatus: {
+    status: number;
+    jailed: boolean;
+  }
+  commission: number;
+  amount: TokenUnit;
+  reward: TokenUnit;
+}
+
+export type RedelegationType = {
+  to: string;
+  from: string;
+  linkedUntil: string;
+  amount: TokenUnit;
+}
+
+export type UnbondingType = {
+  validator: string;
+  commission: number;
+  amount: TokenUnit;
+  linkedUntil: string;
+}
