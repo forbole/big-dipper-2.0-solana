34391b6a8dbc21f797df23af21861f9995629788
yayay927 <isabelleya927@gmail.com>
2022-04-12 16:57:56 +0200

    rmv unused

--- src/screens/epoch/index.tsx
+++ src/screens/epoch/index.tsx
@@ -5,21 +5,9 @@ import {
   Layout,
   LoadAndExist,
 } from '@components';
-// import {
-//   Overview,
-//   Transactions,
-// } from './components';
-// import { useStyles } from './styles';
-// import { useBlockDetails } from './hooks';
 
 const Epoch = () => {
   const { t } = useTranslation('epoch');
-  // const classes = useStyles();
-  // const { state } = useBlockDetails();
-  // const {
-  //   overview,
-  //   transactions,
-  // } = state;
 
   return (
     <>
@@ -34,14 +22,7 @@ const Epoch = () => {
           loading={false}
           exists
         >
-          {/* <span className={classes.root}>
-            <Overview
-              {...overview}
-            />
-            <Transactions
-              transactions={transactions}
-            />
-          </span> */}
+          {}
         </LoadAndExist>
       </Layout>
     </>
