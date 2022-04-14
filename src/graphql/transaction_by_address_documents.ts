647575842a7caac4afa9b4215f29155fd840f163
Wingman <ryuash123@gmail.com>
2022-04-14 11:36:54 -0400

    wip: setting up new query

--- src/graphql/transaction_by_address_documents.ts
+++ src/graphql/transaction_by_address_documents.ts
@@ -1,15 +1,17 @@
 export const TokenAccountUnitDocument = /* GraphQL */`
-query TxByAddress($address: _text) {
-  transactions: instructions_by_address_2_aggregate(args: {addresses: $address, programs: "{}", limit: 10, offset: 0, amount: 100000}, where: {}, distinct_on: tx_signature) {
+query TxByAddress($address: _text, $endSlot: bigint, $startSlot: bigint, $limit: Int = 10, $offset: Int = 0) {
+  instructions: instructions_by_address_aggregate(
+    args: {addresses: $address, programs: "{}", end_slot: $endSlot, start_slot: $startSlot},
+    limit: $limit,
+    offset: $offset,
+    order_by: {slot: desc, tx_signature: asc},
+    distinct_on: tx_signature
+  ) {
     nodes {
       transaction {
-        success
-        slot
         signature
-        numInstructions: num_instructions
-        block {
-          timestamp
-        }
+        slot
+        success
       }
     }
   }
