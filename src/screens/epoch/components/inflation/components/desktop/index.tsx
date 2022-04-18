2e61db51d92cc5fc59d28e620c3aade9661cb06f
yayay927 <isabelleya927@gmail.com>
2022-04-18 14:19:00 +0200

    finish table head in desktop

--- src/screens/epoch/components/inflation/components/desktop/index.tsx
+++ src/screens/epoch/components/inflation/components/desktop/index.tsx
@@ -1,19 +1,59 @@
 import React from 'react';
 import classnames from 'classnames';
-import { Typography } from '@material-ui/core';
 import useTranslation from 'next-translate/useTranslation';
+import { columns } from './utils';
 import { useStyles } from './styles';
 import { InflationType } from '../../../../types';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+} from '@material-ui/core';
 
-const Desktop: React.FC<{inflation: InflationType } & ComponentDefault> = ({
-  className,
-}) => {
+const Desktop: React.FC<{inflation: InflationType } & ComponentDefault> = (props) => {
   const { t } = useTranslation('epoch');
   const classes = useStyles();
+  console.log(props.inflation);
 
   return (
-      <div className={classes.root}>
-        <Typography variant="h4">Desktop</Typography>
+      <div className={classnames(props.className, classes.root)}>
+        <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => {
+              return (
+                <TableCell
+                  key={column.key}
+                  align={column.align}
+                  style={{ width: `${column.width}%` }}
+                >
+                  {t(column.key)}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        {/* <TableBody>
+          {formattedItems.map((row, i) => (
+            <TableRow key={`holders-row-${i}`}>
+              {columns.map((column) => {
+                return (
+                  <TableCell
+                    key={`holders-row-${i}-${column.key}`}
+                    align={column.align}
+                    style={{ width: `${column.width}%` }}
+                  >
+                    {row[column.key]}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody> */}
+      </Table>
       </div>
   );
 };
