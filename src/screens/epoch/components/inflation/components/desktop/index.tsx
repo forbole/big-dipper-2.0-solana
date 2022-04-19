274f11ba09f343b1c7911f547c793a9b8a58134a
yayay927 <isabelleya927@gmail.com>
2022-04-19 16:04:22 +0200

    figure out correct data structure and complete table body in ui

--- src/screens/epoch/components/inflation/components/desktop/index.tsx
+++ src/screens/epoch/components/inflation/components/desktop/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
 import useTranslation from 'next-translate/useTranslation';
-import { columns } from './utils';
-import { useStyles } from './styles';
-import { InflationType } from '../../../../types';
 import {
   Table,
   TableHead,
@@ -12,15 +9,41 @@ import {
   TableBody,
   Typography,
 } from '@material-ui/core';
+import { columns } from './utils';
+import { useStyles } from './styles';
+import { InflationType } from '../../../../types';
 
 const Desktop: React.FC<{inflation: InflationType } & ComponentDefault> = (props) => {
   const { t } = useTranslation('epoch');
   const classes = useStyles();
   console.log(props.inflation);
 
+  const formattedItems = [
+    {
+      name: t('epoch'),
+      value: 301,
+      detail: t('epochDetail'),
+    },
+    {
+      name: t('totalInflation'),
+      value: 0.06987850991688366,
+      detail: t('totalInflationDetail'),
+    },
+    {
+      name: t('validatorInflation'),
+      value: 0.06987850991688366,
+      detail: t('validatorInflationDetail'),
+    },
+    {
+      name: t('foundationInflation'),
+      value: 0,
+      detail: t('foundationInflationDetail'),
+    },
+  ];
+
   return (
-      <div className={classnames(props.className, classes.root)}>
-        <Table>
+    <div className={classnames(props.className, classes.root)}>
+      <Table>
         <TableHead>
           <TableRow>
             {columns.map((column) => {
@@ -36,7 +59,7 @@ const Desktop: React.FC<{inflation: InflationType } & ComponentDefault> = (props
             })}
           </TableRow>
         </TableHead>
-        {/* <TableBody>
+        <TableBody>
           {formattedItems.map((row, i) => (
             <TableRow key={`holders-row-${i}`}>
               {columns.map((column) => {
@@ -52,9 +75,9 @@ const Desktop: React.FC<{inflation: InflationType } & ComponentDefault> = (props
               })}
             </TableRow>
           ))}
-        </TableBody> */}
+        </TableBody>
       </Table>
-      </div>
+    </div>
   );
 };
 
