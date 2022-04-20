a2730fc8facbae0d3d937a700465a0dd7844986c
Ryuash <42913823+ryuash@users.noreply.github.com>
2022-04-20 15:39:49 -0400

    wip: setup queries (#167)

--- src/screens/account_details/components/transactions/hooks.ts
+++ src/screens/account_details/components/transactions/hooks.ts
@@ -1,47 +1,82 @@
-import { useState } from 'react';
-// import * as R from 'ramda';
-// import { useRouter } from 'next/router';
-// import {
-//   useTxByAddressQuery, TxByAddressQuery,
-// } from '@graphql/types';
+import {
+  useEffect, useState,
+} from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { LatestBlockHeightDocument } from '@src/graphql/block_height_documents';
+import { TxByAddressDocument } from '@graphql/transaction_by_address_documents';
+import * as R from 'ramda';
 import { TransactionsState } from './types';
 
 export const useTransactions = () => {
-  // const router = useRouter();
-  const [state, _setState] = useState<TransactionsState>({
+  const router = useRouter();
+  const [state, setState] = useState<TransactionsState>({
     loading: true,
     transactions: [],
+    startSlot: 0,
+    endSlot: 0,
+    total: 0,
   });
 
-  // const handleSetState = (stateChange: any) => {
-  //   setState((prevState) => R.mergeDeepLeft(stateChange, prevState));
-  // };
+  useEffect(() => {
+    init();
+  }, []);
 
-  // useTxByAddressQuery({
-  //   variables: {
-  //     address: `{${router.query.address}}`,
-  //   },
-  //   onCompleted: (data) => {
-  //     const transactions = formatTransactions(data);
-  //     handleSetState({
-  //       loading: false,
-  //       transactions,
-  //     });
-  //   },
-  // });
+  const handleSetState = (stateChange: any) => {
+    setState((prevState) => R.mergeDeepLeft(stateChange, prevState));
+  };
 
-  // const formatTransactions = (data: TxByAddressQuery) => {
-  //   return data.transactions.nodes.map((x) => {
-  //     const { transaction } = x;
-  //     return ({
-  //       slot: transaction.slot,
-  //       signature: transaction.signature,
-  //       success: transaction.success,
-  //       timestamp: R.pathOr('', ['block', 'timestamp'], transaction),
-  //       numInstructions: R.pathOr(0, ['numInstructions'], transaction),
-  //     });
-  //   });
-  // };
+  // get latest block height
+  const init = async () => {
+    try {
+      const { data } = await axios.post(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
+        query: LatestBlockHeightDocument,
+      });
+      const endSlot = R.pathOr(0, ['data', 'height', 0, 'slot'], data);
+      const startSlot = endSlot - 10000; // recommended search interval
+
+      handleSetState({
+        startSlot,
+        endSlot,
+      });
+
+      await getTransactions(startSlot, endSlot);
+    } catch (error) {
+      handleSetState({
+        loading: false,
+      });
+    }
+  };
+
+  const getTransactions = async (startSlot: number, endSlot: number) => {
+    const { data } = await axios.post(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
+      variables: {
+        address: `{${router.query.address}}`,
+        startSlot,
+        endSlot,
+      },
+      query: TxByAddressDocument,
+    });
+    const transactions = R.pathOr([], ['data', 'instructions', 'nodes'], data);
+    const formatted = formatTransactions(transactions);
+    handleSetState({
+      loading: false,
+      transactions: formatted,
+    });
+  };
+
+  const formatTransactions = (data: any[]) => {
+    return data.map((x) => {
+      const { transaction } = x;
+      return ({
+        slot: transaction.slot,
+        signature: transaction.signature,
+        success: transaction.success,
+        timestamp: R.pathOr('', ['block', 'timestamp'], transaction),
+        numInstructions: R.pathOr(0, ['numInstructions'], transaction),
+      });
+    });
+  };
 
   return {
     state,
--- src/screens/account_details/components/transactions/types.ts
+++ src/screens/account_details/components/transactions/types.ts
@@ -1,4 +1,7 @@
 export type TransactionsState = {
   loading: boolean;
   transactions: Transactions[];
+  startSlot: number;
+  endSlot: number;
+  total: number;
 }
