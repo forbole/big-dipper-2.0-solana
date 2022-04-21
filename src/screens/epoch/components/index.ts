2402fa3ebd567adcf69788fb78ce6a8f098288c9
yayay927 <isabelleya927@gmail.com>
2022-04-21 23:19:59 +0200

    delete original 3 components files and update components/index

--- src/screens/epoch/components/index.ts
+++ src/screens/epoch/components/index.ts
@@ -1,11 +1,5 @@
-import Inflation from './inflation';
-import InflationGovernor from './inflation_governor';
-import EpochSchedule from './epoch_schedule';
 import EpochDetail from './epochDetail';
 
 export {
-  Inflation,
-  InflationGovernor,
-  EpochSchedule,
   EpochDetail,
 };
