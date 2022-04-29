12e98e1754aef4838784a3b4b34f4858902dab93
ryuash <ryuash123@gmail.com>
2022-04-29 09:37:40 -0400

    update: tx

--- src/screens/account_details/components/transactions/hooks.ts
+++ src/screens/account_details/components/transactions/hooks.ts
@@ -3,7 +3,6 @@ import {
 } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { LatestBlockHeightDocument } from '@src/graphql/block_height_documents';
 import { TxByAddressDocument } from '@graphql/transaction_by_address_documents';
 import * as R from 'ramda';
 import { TransactionsState } from './types';
@@ -13,9 +12,6 @@ export const useTransactions = () => {
   const [state, setState] = useState<TransactionsState>({
     loading: true,
     transactions: [],
-    startSlot: 0,
-    endSlot: 0,
-    total: 0,
   });
 
   useEffect(() => {
@@ -29,19 +25,11 @@ export const useTransactions = () => {
   // get latest block height
   const init = async () => {
     try {
-      const { data } = await axios.post(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
-        query: LatestBlockHeightDocument,
-      });
-      const endSlot = R.pathOr(0, ['data', 'height', 0, 'slot'], data);
-      const startSlot = endSlot - 10000; // recommended search interval
-
-      const transactions = await getTransactions(startSlot, endSlot);
+      const transactions = await getTransactions();
 
       handleSetState({
         loading: false,
         transactions,
-        startSlot,
-        endSlot,
       });
     } catch (error) {
       handleSetState({
@@ -50,22 +38,21 @@ export const useTransactions = () => {
     }
   };
 
-  const getTransactions = async (startSlot: number, endSlot: number) => {
+  const getTransactions = async (current = '') => {
     const { data } = await axios.post(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
       variables: {
-        address: `{${router.query.address}}`,
-        startSlot,
-        endSlot,
+        address: router.query.address as string,
+        current,
+        limit: 50 + 1,
       },
       query: TxByAddressDocument,
     });
-    const transactions = R.pathOr([], ['data', 'instructions', 'nodes'], data);
+    const transactions = R.pathOr([], ['data', 'transactionsByAddress'], data);
     return formatTransactions(transactions);
   };
 
   const formatTransactions = (data: any[]) => {
-    return data.map((x) => {
-      const { transaction } = x;
+    return data.map((transaction) => {
       return ({
         slot: transaction.slot,
         signature: transaction.signature,
