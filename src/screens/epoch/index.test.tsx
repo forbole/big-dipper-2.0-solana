61ccc6902f7c90de32ffe0e6af8d870140d85424
yayay927 <isabelleya927@gmail.com>
2022-05-02 15:17:16 +0200

    finally fixed the apollo error about Request handler not defined for query

--- src/screens/epoch/index.test.tsx
+++ src/screens/epoch/index.test.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable */
 import React from 'react';
-import { RecoilRoot } from 'recoil';
 import renderer from 'react-test-renderer';
 import { createMockClient } from 'mock-apollo-client';
 import { ApolloProvider } from '@apollo/client';
 import {
   MockTheme, wait,
 } from '@tests/utils';
-import { EpochDocument } from '@graphql/types';
+import { EpochDetailsDocument } from '@graphql/types';
 import Epoch from '.';
 
 // ==================================
@@ -22,37 +21,30 @@ jest.mock('./components', () => ({
   EpochDetail: (props) => <div id="EpochDetail" {...props} />,
 }));
 
-const mockEpochDocument = jest.fn().mockResolvedValue({
+const mockEpochDetailsDocument = jest.fn().mockResolvedValue({
   "data": {
-    "inflation": {
-        "epoch": 305,
-        "foundation": 0,
-        "total":0.06963020582432754,
-        "validator": 0.06963020582432754,
+    "inflationRate": {
+      "epoch": 305,
+      "foundation": 0,
+      "total":0.06963020582432754,
+      "validator": 0.06963020582432754,
     },
-    "inflationGovernon": {
-        "foundation": 0,
-        "foundationTerm": 0,
-        "initial": 0.08,
-        "taper": 0.15,
-        "terminal": 0.015,
+    "inflationGovernor": {
+      "foundation": 0,
+      "foundationTerm": 0,
+      "initial": 0.08,
+      "taper": 0.15,
+      "terminal": 0.015,
     },
     "epochSchedule": {
-        "firstNormalEpoch": 0,
-        "firstNormalSlot": 0,
-        "leaderScheduleSlotOffset": 432000,
-        "slotsPerEpoch": 432000,
-        "warmup": false,
+      "firstNormalEpoch": 0,
+      "firstNormalSlot": 0,
+      "leaderScheduleSlotOffset": 432000,
+      "slotsPerEpoch": 432000,
+      "warmup": false,
     }
   }
 });
-
-
-epochSchedule:
-
-
-
-
 
 
 // ==================================
@@ -63,21 +55,21 @@ describe('screen: Epoch', () => {
     const mockClient = createMockClient();
 
     mockClient.setRequestHandler(
-      EpochDocument,
-      mockEpochDocument,
+      EpochDetailsDocument,
+      mockEpochDetailsDocument,
     );
+
+console.log('mockEpochDocument', mockEpochDetailsDocument)
 
     let component;
 
     renderer.act(() => {
       component = renderer.create(
-        <RecoilRoot>
           <ApolloProvider client={mockClient}>
             <MockTheme>
               <Epoch />
             </MockTheme>
           </ApolloProvider>
-        </RecoilRoot>,
       );
     });
     await wait();
